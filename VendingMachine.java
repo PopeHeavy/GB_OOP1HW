08ee984fabd1b6b979fcdcb395e3dace1ca98a25
Alexander Grigoriev <135022555+PopeHeavy@users.noreply.github.com>
2023-11-26 17:47:23 +0300

    Add files via upload

---
+++ VendingMachine.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public interface VendingMachine {
+	
+	
+	public void initBeverages(List<Beverages> beveragesList);
+	
+	public Beverages getBeverages(String name);
+
+}
